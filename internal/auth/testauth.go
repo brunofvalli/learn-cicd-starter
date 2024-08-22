19b000a78c3e44a9af5f36adccffad582de94598
Bruno <brunofvalli@gmail.com>
2024-08-22 09:40:47 -0500

    Fixed the test in Go

--- internal/auth/testauth.go
+++ internal/auth/testauth.go
@@ -1,10 +1,14 @@
 package split
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-    
+	got := Split("a/b/c", "/")
+	want := []string{"a", "b", "c"}
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
 }
